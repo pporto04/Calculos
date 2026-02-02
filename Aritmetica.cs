98f407f690a65386705d2ab85c3f1be7b18aa25b
Paula Porto <170345196+pporto04@users.noreply.github.com>
2026-02-02 17:47:41 +0000

    Adicionar método converter temperatura

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,8 +9,18 @@ namespace Calculos
     /// <summary>
     /// Implementa operações Aritméticas
     /// </summary>
+  
     internal class Aritmetica
     {
+        /// <summary>
+        /// Tipo de conversão de temperatura a executar
+        /// </summary>
+        public enum ConversaoTemperatura
+        { 
+            Nulo = 0,
+            CelsiusFahrenheit,
+            FahrenneitCelsius
+        }
         /// <summary>
         /// Operação Soma.
         /// </summary>
@@ -27,6 +37,19 @@ namespace Calculos
         {
             return x - y;
         }
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelcius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
+            return -1;
+        }
+      
 
     }
 }
